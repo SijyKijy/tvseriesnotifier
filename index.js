cca1955f50382f0a3090c9966f104e86fffe4f5b
SijyKijy <SijyKijy@users.noreply.github.com>
2021-01-16 00:00:04 +0300

    modify

--- index.js
+++ index.js
@@ -77,9 +77,6 @@ function PostToDiscordWebHook(content) {
     };
 
     var req = https.request(options, (res) => {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
-
         res.on('data', (d) => {
             process.stdout.write(d);
         });
